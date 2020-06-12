2cc01f3574c89f0db3536cd2849257349e034e9b
leo7535 <leo7535@naver.com>
2020-06-13 01:10:30 +0900

    6/13 commit

--- main.js
+++ main.js
@@ -3,8 +3,20 @@ $(document).ready(function () {
   paper.install(window);
   paper.setup(document.getElementById("mainCanvas"));
 
-  var c = Shape.Circle(200, 200, 50);
-  c.fillColor = "green";
+  var tool = new paper.Tool();
+
+  var c = Shape.Circle(200, 200, 80);
+  c.fillColor = "black";
+  var text = new PointText(200, 200);
+  text.justification = "center";
+  text.fillColor = "white";
+  text.fontSize = "20";
+  text.content = "hello world";
+
+  tool.onMouseDown = function (event) {
+    var c = Shape.Circle(event.point.x, event.point.y, 20);
+    c.fillColor = "green";
+  };
 
   paper.view.draw();
 });
