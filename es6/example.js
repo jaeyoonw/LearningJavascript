c8df02d731fd320ee4b5b1f365d3f234f997a747
김재윤 <kimjaeyoon@gimjaeyun-ui-MacBookPro.local>
2020-06-29 20:44:55 +0900

    examples

---
+++ es6/example.js
@@ -0,0 +1,14 @@
+const dialog =
+  'Sam looked up, and said "hello, old friend!", as Max walked in.';
+const dialog2 = 'Sam looked up,˜ and said "don\'t do that!" to Max.';
+const imperative = "Don't do that!";
+const s = "In Javascript, use \\ as an escape character in strings";
+
+let currentTemp = 19.5;
+const message = `The current temperature is ${currentTemp}\u00b0C`;
+
+console.log(dialog);
+console.log(dialog2);
+console.log(imperative);
+console.log(s);
+console.log(message);
